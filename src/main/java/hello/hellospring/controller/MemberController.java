8135e4028819c4bb102bceca95a9f01f82030e7d
min2oyo <min2u@outlook.com>
2022-07-07 14:20:01 +0900

    회원 웹 기능 | 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 스프링이 뜰 때 스프링 컨테이너가 생기고 아래 객체를 생성해서 담아두고 관리함
 public class MemberController {
@@ -13,7 +16,23 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member: " + form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 
 }
